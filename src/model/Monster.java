1b08c1cba72e3e7357719600f30fa39f021e5321
Chantal <chantalforget3@gmail.com>
2015-02-04 16:27:56 -0500

    new file:   Monster.java

---
+++ src/model/Monster.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Monster {
+	Clearing location; 
+	
+	public void move(Clearing location) {
+		
+	}
+	
+	// also includes blocking a player
+	public void appear() {
+		
+	}
+	
+}
