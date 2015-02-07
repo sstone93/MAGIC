47c594ed74d8480ea54d26610fdf5beb9db21fb0
Chantal <chantalforget3@gmail.com>
2015-02-06 19:20:47 -0500

    added visible boolean for chit and gold functions

--- src/model/Chit.java
+++ src/model/Chit.java
@@ -1,16 +1,25 @@
 package model;
 
 public class Chit {
-	String name; 
-	Clearing location;
-	// also want the image, not sure what it should be stored as
-	
-	public String getName() {
-		return name; 
-	}
-	
-	public void setLocation(Clearing newLocation) {
-		location = newLocation;
-	}
-	
+    String   name;
+    Clearing location;
+    boolean  visible;
+    // also want the image, not sure what it should be stored as
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLocation(Clearing newLocation) {
+        location = newLocation;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
 }
