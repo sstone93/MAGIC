c067230aaecd3be91aafb3d262d1a793bd3e29b6
Chantal <chantalforget3@gmail.com>
2015-02-09 14:15:26 -0500

    added in weapon values as in issue #4

--- src/model/Weapon.java
+++ src/model/Weapon.java
@@ -3,23 +3,33 @@ import Utility;
 
 public class Weapon {
     boolean ranged;
-    int     length;
-    int     weight;
+    boolean active;
+    String  attack; // the method of attack
+    int     length; // on a scale of 0 - 18
     int     speed;
+    int     sharpness; // higher = more harm inflicted
+    WeaponDamage weight;
     WeaponDamage damage;
 
     public void setRanged(boolean ranged) {
         this.ranged = ranged;
     }
-    public void isRanged() {
+    public boolean isRanged() {
         return ranged;
     }
 
-    public WeaponDamage getDamage() {
-        return damage;
+    public void setActive(boolean active) {
+        this.active = active;
     }
-    public WeaponDamage setDamage(WeaponDamage damage) {
-        this.damage = damage;
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setAttack(boolean attack) {
+        this.attack = attack;
+    }
+    public String getAttack() {
+        return attack;
     }
 
     public void getLength() {
@@ -38,4 +48,25 @@ public class Weapon {
         this.speed = speed;
     }
 
+    public void setSharpness(int sharp) {
+        sharpness = sharp;
+    }
+
+    public int getSharpness() {
+        return sharpness;
+    }
+
+    public WeaponDamage getWeight() {
+        return weight;
+    }
+    public WeaponDamage setWeight(WeaponDamage weight) {
+        this.weight = weight;
+    }
+
+    public WeaponDamage getDamage() {
+        return damage;
+    }
+    public WeaponDamage setDamage(WeaponDamage damage) {
+        this.damage = damage;
+    }
 }
