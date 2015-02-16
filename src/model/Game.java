89ad158065fe69f7400ec8c4ef2f7e4b1303d436
michaelcatt <lemmingllama@gmail.com>
2015-02-16 12:39:29 -0500

    Changed startDay in Game.java so it would choose the proper ordering and calls other functions in order.
    It looks like the roll function is only needed in Game.java, we could
    likely move it from Utility.java.
    
    Also I'd like to propose that we make resetDay call startDay after
    resetting and also resetting week if necessary.

--- src/model/Game.java
+++ src/model/Game.java
@@ -77,7 +77,54 @@ public class Game {
 
     public void startDay() {
         currentDay++;
-
+        
+        //TODO choose moves
+        
+        // Silly way to order players from 1 to playerCount+1
+        for (int i = 0; i <= playerCount; i++) {
+        	players[i].order = roll(100);
+        }
+        int[] ordering = new int[playerCount];
+        for (int i = 0; i <= playerCount; i++) {
+        	ordering[i] = players[i].order;
+        }
+        Arrays.sort(ordering);
+        for (int i = 0); i <= playerCount; i++) {
+        	for (int j = 0); j <= playerCount; j++) {
+        		if (ordering[i] == players[j].order) {
+        			players[j].order = i;
+        			break;
+        		}
+        	}
+        }
+        
+        // Do moves in order
+        int nextMover = 0;
+        while (nextMover <= playerCount) {
+        	for (int i = 0; i <= playerCount; i++) {
+        		if (players[i].order == nextMover) {
+        			//TODO player does their moves in order
+        			nextMover++;
+        			break;
+        		}
+        	}
+        }
+        
+        // Choose attacks
+        nextMover = 0;
+        while (nextMover <= playerCount) {
+        	for (int i = 0; i <= playerCount; i++) {
+        		if (players[i].order == nextMover) {
+        			// TODO choose attackers and save somewhere
+        			nextMover++;
+        			break;
+        		}
+        	}
+        }
+        
+        // TODO combat loop
+        
+        resetDay(); // not sure if we want to change resetDay so it will call startDay again if all is well
     }
 
     // todo: in next iteration calculate the score properly
