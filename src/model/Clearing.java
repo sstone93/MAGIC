417e4872983c6a905b0ee410386afcfad0be2bfe
Nick <nlaws101@gmail.com>
2015-03-02 13:37:27 -0500

    updated clearing class to ArrayLists

--- src/model/Clearing.java
+++ src/model/Clearing.java
@@ -1,8 +1,7 @@
 package model;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import utils.Config;
+import java.util.ArrayList;
 
 public class Clearing implements Serializable{
 	
@@ -10,86 +9,37 @@ public class Clearing implements Serializable{
 	public String type;
     public Garrison dwelling = null; 			// not sure what type this should actually be
     public int location;		 		// indicating which clearing on the tile this is CHANGED BACK TO AN INT
-    public int numberOfTreasures = 0;
-    public Clearing[] connections;
-    public int nextConnection =0;
     public Tile parent;
-    public Treasure[] treasures;
-    public Player[] occupants;
-    public int nextOccupant = 0;
-
+    ArrayList<Clearing> connections = new ArrayList<Clearing>();
+    ArrayList<Treasure> treasures = new ArrayList<Treasure>();
+    ArrayList<Player> occupants = new ArrayList<Player>();
+    
     Clearing(int location, Tile parent) {
         this.type     = "";
         this.location = location;
         this.parent   = parent;
-        connections   = new Clearing[4];		//4 is the most connections had by any
-        treasures     = new Treasure[10]; // arbitrary number
-        this.occupants = new Player[Config.MAX_CLIENTS];
-       // occupants = 
-        // TODO: how should the treasures be added to the tile?
     }
 
     public void addOccupant(Player p){
-    	System.out.println(this);
     	System.out.println("adding player" + p);
-    	System.out.println(nextOccupant);
-    	occupants[nextOccupant] = p;
-    	nextOccupant += 1;
-    	System.out.println(this.occupants);
+    	occupants.add(p);
     }
     
-    //This will be removed once we change to ArrayLists, I just needed to test the GUI.
+    /**
+     * 
+     * @param p
+     */
     public void removeOccupant(Player p){
-    	System.out.println(this);
     	System.out.println("removing player" + p);
-    	int count = 0;
-    	while (count < nextOccupant){
-    		if(occupants[count] == p){
-    			System.out.println("found player to remove");
-    			if(count != nextOccupant - 1){
-	    			int countDown = nextOccupant - 2;
-	    			while (countDown >= count){
-	    				occupants[countDown] = occupants[countDown + 1];
-	    				countDown--;
-	    			}
-    			}
-    			occupants[nextOccupant - 1] = null;
-    			nextOccupant--;
-    		}
-    		count++;
-    	}
+    	occupants.remove(p);
     }
     
     /**
      * Returns all the players currently in the clearing
      * @return List of Players in the clearing
      */
-    public Player[] getOccupants(){
+    public ArrayList<Player> getOccupants(){
     	return this.occupants;
-    }
-    
-    /**
-     * Move the player p into this clearing
-     * @param Player moving into the clearing
-     */
-    public void moveIn(Player p){
-    	p.getLocation().moveOut(p);
-    	p.setLocation(this);
-//    	this.addOccupant(p);
-//    	this.nextOccupant += 1;	//TODO THIS IS A PROBLEM, TOO MANY MOVES IN AND OUT DESTROY THE ARRAY, NEED AN ARAY LIST!!!!!!!!!!11
-    }
-    
-    /**
-     * Moves player p out of the cleaing
-     * @param p Player to be moved out
-     */
-    public void moveOut(Player p){
-    	for(int i=0; i< occupants.length;i++){
-    		if(occupants[i] == p){
-    			occupants[i] = null;
-    			nextOccupant = i; //TODO THIS IS A PROBLEM. REALLY WANT AN ARRAY LIST
-    		}
-    	}
     }
     
     /**
@@ -97,8 +47,7 @@ public class Clearing implements Serializable{
      * @param toAdd The clearing being added to this one as a connection
      */
     public void addConnection(Clearing toAdd){
-    	connections[nextConnection] = toAdd;
-    	nextConnection += 1;
+    	this.connections.add(toAdd);
     }
 
     public String getType() {
@@ -122,40 +71,20 @@ public class Clearing implements Serializable{
     }
 
     public boolean canChangeClearing(Clearing clearing) {
-        // if they're connected
-    	
-        for (int i = 0; i < 4; i++) {
-        	if (connections[i] != null) {
-	        	if (clearing.getClearingNumber() == connections[i].getClearingNumber() ) 
-	        		if (clearing.parent.getName() == connections[i].parent.getName())
-	        			return true;
-        	}
-        } 
-        return false;
+    	if(connections.contains(clearing)){
+    		return true;
+    	}else{
+    		return false;
+    	}
     }
 
-    public void addTreasures(Treasure[] treasure) {
-        treasures = treasure;
-        numberOfTreasures = treasure.length;
+    public void addTreasures(ArrayList<Treasure> treasure) {
+        this.treasures.addAll(treasure);
     }
 
-    // removes treasure from the clearing
-    // used when a player finds the treasure
-    public void removeTreasure(Treasure treasure) {
-        for (int j = 0; j < numberOfTreasures - 1; j++) {
-            treasures[j] = treasures[j+1];
-        }
-        numberOfTreasures--;
-    }
-
-    public Treasure[] getTreasures() {
+    public ArrayList<Treasure> getTreasures() {
         return treasures;
     }
-
-    public int getNumberOfTreasures() {
-        return numberOfTreasures;
-    }
-
 
     @Override
 	public String toString(){
@@ -164,14 +93,14 @@ public class Clearing implements Serializable{
     		System.out.println("		-has garrison: "+dwelling.getName());
     		System.out.println("		"+dwelling);
     	}
-    	for(int i=0;i<occupants.length;i++){
-			if(occupants[i] != null){
-				System.out.println("		-contains Player "+occupants[i].getID());
+    	for(int i=0;i<occupants.size();i++){
+			if(occupants.get(i) != null){
+				System.out.println("		-contains Player "+occupants.get(i).getID());
 			}
 		}
-		for(int i=0;i<connections.length;i++){
-			if(connections[i] != null){
-				System.out.println("		-connected to "+connections[i].parent.getName()+" clearing #"+connections[i].location);
+		for(int i=0;i<connections.size();i++){
+			if(connections.get(i) != null){
+				System.out.println("		-connected to "+connections.get(i).parent.getName()+" clearing #"+connections.get(i).location);
 			}
 		}
 		return "";
