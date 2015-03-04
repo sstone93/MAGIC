a7084ecd8a77f0e15a81f2eebe7f5704b66bf0a4
Nick <nlaws101@gmail.com>
2015-03-04 15:49:47 -0500

    added window close detect

--- src/view/View.java
+++ src/view/View.java
@@ -29,6 +29,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.HashMap;
 
 @SuppressWarnings("serial")
@@ -94,6 +96,14 @@ public class View extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		this.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                System.out.println("Closed Window");
+                System.exit(0);
+            }
+        } );
+
 		JPanel CharacterInfoPanel = new JPanel();
 		CharacterInfoPanel.setBounds(819, 0, 455, 500);
 		CharacterInfoPanel.setBorder(new LineBorder(Color.GRAY));
