9c75956c0e5496352126c0e4ab70d0a77ae747ed
michaelcatt <lemmingllama@gmail.com>
2015-03-20 13:18:31 -0400

    Added monster initialization to monster class

--- src/model/Monster.java
+++ src/model/Monster.java
@@ -23,6 +23,66 @@ public class Monster implements Serializable{
 	CombatMoves moves;
     // TODO put weight, in enum
 
+	Monster(MonsterName name) {
+		this.name = name;
+		if (name == MonsterName.GHOST) {
+			this.weight       = ItemWeight.MEDIUM;
+			this.notoriety    = 2;
+			this.fame         = 0;
+			this.armoured     = false;
+			this.attackLength = 2;
+			this.attackSpeed  = 2;
+		}
+		else if (name == MonsterName.GIANT) {
+			this.weight       = ItemWeight.TREMENDOUS;
+			this.notoriety    = 8;
+			this.fame         = 8;
+			this.armoured     = false;
+			this.attackLength = 5;
+			this.attackSpeed  = 5;
+		}
+		else if (name == MonsterName.HEAVY_DRAGON) {
+			this.weight       = ItemWeight.HEAVY;
+			this.notoriety    = 5;
+			this.fame         = 5;
+			this.armoured     = true;
+			this.attackLength = 4;
+			this.attackSpeed  = 4;
+		}
+		else if (name == MonsterName.HEAVY_TROLL) {
+			this.weight       = ItemWeight.HEAVY;
+			this.notoriety    = 5;
+			this.fame         = 5;
+			this.armoured     = true;
+			this.attackLength = 4;
+			this.attackSpeed  = 4;
+		}
+		else if (name == MonsterName.VIPER) {
+			this.weight       = ItemWeight.MEDIUM;
+			this.notoriety    = 2;
+			this.fame         = 1;
+			this.armoured     = true;
+			this.attackLength = 4;
+			this.attackSpeed  = 4;
+		}
+		else if (name == MonsterName.WOLF) {
+			this.weight       = ItemWeight.MEDIUM;
+			this.notoriety    = 1;
+			this.fame         = 0;
+			this.armoured     = false;
+			this.attackLength = 3;
+			this.attackSpeed  = 4;
+		}
+		else {
+			this.weight       = ItemWeight.NEGLIGIBLE;
+			this.notoriety    = 0;
+			this.fame         = 0;
+			this.armoured     = false;
+			this.attackLength = 1;
+			this.attackSpeed  = 1;
+		}
+	}
+	
 	public MonsterName getName() {
 		return name;
 	}
